f4181bcd1484607e0e2514f3e48b9de8f792c0d3
Sunny Jha <sunnyjha11897@gmail.com>
2020-11-09 01:52:13 +0530

    [SUNNY]ADD.TestCase to retrive the employees

---
+++ RestSharpTest/RestSharpTestCase.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+
+namespace RestSharpTest
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+    [TestClass]
+    public class RestSharpTestCase
+    {
+        RestClient client;
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:4000");
+        }
+
+        private IRestResponse GetEmployeeList()
+        {
+            // arrange
+            RestRequest request = new RestRequest("/employees", Method.GET);
+
+            // act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        [TestMethod]
+        public void OnCallingGETApi_ReturnEmployeeList()
+        {
+            IRestResponse response = GetEmployeeList();
+
+            // assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(3, dataResponse.Count);
+        }
+
+    }
+}
